434760552b402ac557254111d3d521460749cdc2
DenislavNedev <dnedev98@gmail.com>
2020-12-11 19:08:12 +0200

    Innit commit

---
+++ src/main/java/com/uni/sofia/schedule/it/ScheduleItApplication.java
@@ -0,0 +1,13 @@
+package com.uni.sofia.schedule.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/sofia/schedule/it/ScheduleItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.sofia.schedule.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
